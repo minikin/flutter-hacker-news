b1102cc2c756f18c2e09e763efd074981ed7e6c8
Sasha Prokhorenko <djminikin@gmail.com>
2019-02-27 16:11:39 +0100

    Add analysis_options

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_hacker_news/main.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_hacker_news/main.dart';
 
 void main() {}
